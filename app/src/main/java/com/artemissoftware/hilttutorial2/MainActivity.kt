308c8de610c8380bdf161550dacf358bcfbb4202
gustavo <gustavo.isaac@gmail.com>
2021-05-12 15:42:10 +0100

    Injecting into Activities

--- app/src/main/java/com/artemissoftware/hilttutorial2/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/hilttutorial2/MainActivity.kt
@@ -2,10 +2,21 @@ package com.artemissoftware.hilttutorial2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var testString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Test String from MainActivity: $testString")
+
     }
 }
