6b149b84b258f97c29c56c08ae89cb0d0f42ed44
gustavo <gustavo.isaac@gmail.com>
2021-05-12 15:58:39 +0100

    Providing dependencies that need other dependencies

--- app/src/main/java/com/artemissoftware/hilttutorial2/MainModule.kt
+++ app/src/main/java/com/artemissoftware/hilttutorial2/MainModule.kt
@@ -1,10 +1,12 @@
 package com.artemissoftware.hilttutorial2
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Named
 import javax.inject.Singleton
@@ -16,5 +18,8 @@ object MainModule {
     @ActivityScoped
     @Provides
     @Named("String2")
-    fun provideTestString2() = "This is a string we will inject as well"
+    fun provideTestString2(
+        @ApplicationContext context: Context,
+        @Named("String1") testString1: String
+    ) = "${context.getString(R.string.string_to_inject)} - $testString1"
 }
