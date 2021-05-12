f991a1785e217306f4b166e9715a343d2c69279a
gustavo <gustavo.isaac@gmail.com>
2021-05-12 16:03:27 +0100

    Injecting into ViewModels

--- app/src/main/java/com/artemissoftware/hilttutorial2/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/hilttutorial2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.artemissoftware.hilttutorial2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import javax.inject.Named
@@ -18,12 +19,18 @@ class MainActivity : AppCompatActivity() {
     @Named("String2")
     lateinit var testString2: String
 
+
+    private val viewModel: TestViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", "Test String from MainActivity: $testString")
         Log.d("MainActivity", "Another Test String from MainActivity: $testString2")
+
+        viewModel
 
     }
 }
